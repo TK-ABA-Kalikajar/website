3ec024be47f0c02f30f8043c71c5f12711185a8a
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2026-01-12 09:07:03 +0700

    Refactor code structure for improved readability and maintainability

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://tkabakalikajar.com',
+  site: 'https://tkabakalikajar.sch.id',
 });
